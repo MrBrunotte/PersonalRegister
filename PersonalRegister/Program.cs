0462aafcd3141a8aa7f1992d6d1a7aad7efd06e0
mrbrunotte <mrbrunotte@gmail.com>
2020-08-25 16:08:25 +0200

    Add project files.

---
+++ PersonalRegister/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Versioning;
+
+namespace PersonalRegister
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            personal();
+        }
+        
+        static void personal()
+        {
+            List<string> personal = new List<string>();
+
+            personal.Add("Stefan Brunotte 150000");
+            personal.Add("Stefan Brunotte 160000");
+            personal.Add("Stefan Brunotte 170000");
+            personal.Add("Stefan Brunotte 180000");
+
+            for (int i = 0; i < personal.Count; i++)
+            {
+                Console.WriteLine(personal[i]);
+            }
+        }
+    }
+
+     
+}
